071443320ba132ac6d84b2228c0ff514b8046e86
Maycdouglas <mayconcruzeiroraposa@gmail.com>
2025-06-07 08:43:19 -0300

    insere constantes globais na classe Token

---
+++ src/main/java/Token.java
@@ -0,0 +1,14 @@
+public class Token {
+
+    // static + final cria uma constante global
+    public static final int VAR = 1; // a,b,c...
+    public static final int INT = 2; // 1,2,3...
+    public static final int EQ = 3; // =
+    public static final int SEMI = 4; // ;
+    public static final int PLUS = 5; // +
+    public static final int MULT = 6; // *
+    public static final int EOF = -1; // end of line
+
+
+}
+
