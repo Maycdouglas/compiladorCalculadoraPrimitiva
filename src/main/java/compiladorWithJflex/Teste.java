bbad9faa25c42768f792e1fb61c446e03542ad69
Maycdouglas <mayconcruzeiroraposa@gmail.com>
2025-06-08 12:45:00 -0300

    adiciona package compiladorWithJflex com classe Teste e ENUM TOKEN_TYPE

---
+++ src/main/java/compiladorWithJflex/Teste.java
@@ -0,0 +1,18 @@
+package compiladorWithJflex;
+
+import java.io.FileReader;
+import java.io.IOException;
+
+public class Teste {
+
+    public static void main(String[] args) throws IOException {
+        LextTest = new LextTest(new FileReader(args[0]));
+        Token t = lx.nextToken();
+        while(t != null){
+            System.out.println(t.toString());
+            t = lx.nextToken();
+        }
+        System.out.println("Total de tokens lidos " + lx.readedTokens());
+    }
+
+}
